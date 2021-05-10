e02e12e29f6e41687f1a6db9f63f68f85ede6a12
karolis-vegvisir <karolis.blazevicius@vegvisir.lt>
2021-05-10 17:18:13 +0300

    Cleanup.

--- src/AspNetBasic.WebApi/Startup.cs
+++ src/AspNetBasic.WebApi/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
@@ -20,7 +19,6 @@ namespace AspNetBasic.WebApi
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
