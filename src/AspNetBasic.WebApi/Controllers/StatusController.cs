d569677b5ff0c4a67fd4bb5788e9715c52239936
karolis-vegvisir <karolis.blazevicius@vegvisir.lt>
2021-05-10 16:28:28 +0300

    Add StatusController.

---
+++ src/AspNetBasic.WebApi/Controllers/StatusController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetBasic.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet("")]
+        public ActionResult GetStatus()
+        {
+            return Ok("Hello from AspNetBasic!");
+        }
+    }
+}
